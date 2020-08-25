fde3b90985a3a4e237bdfae6b0dfdc6b2b6203f0
Roman Bondarenko <rom_bon@mail.ru>
2020-08-25 19:38:10 +0300

    Base empty WindowResizeObserver added.

---
+++ engine/main/view/windows/base/observers/WindowResizeObserver.cpp
@@ -0,0 +1,11 @@
+//
+// Created by roman on 25.08.2020.
+//
+
+#include "WindowResizeObserver.h"
+
+void WindowView::WindowResizeObserver::update(const sf::Event &event, sf::RenderWindow &window) noexcept {
+    if (event.type == sf::Event::Resized) {
+        //..
+    }
+}
---
+++ engine/main/view/windows/base/observers/WindowResizeObserver.h
@@ -0,0 +1,19 @@
+//
+// Created by roman on 25.08.2020.
+//
+
+#ifndef FLASH_WINDOWRESIZEOBSERVER_H
+#define FLASH_WINDOWRESIZEOBSERVER_H
+
+#include <SFML/Graphics/RenderWindow.hpp>
+
+#include "../observer/Observer.h"
+
+namespace WindowView {
+    class WindowResizeObserver : public Observer<sf::RenderWindow, sf::Event> {
+    public:
+        void update(const sf::Event &event, sf::RenderWindow &window) noexcept override;
+    };
+}
+
+#endif //FLASH_WINDOWRESIZEOBSERVER_H
