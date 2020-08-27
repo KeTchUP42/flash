640858554299c0ea0a31a29744318e9b8c8d74c6
Roman Bondarenko <rom_bon@mail.ru>
2020-08-27 18:03:13 +0300

    Movable capability added.

---
+++ engine/main/objects/capabilities/Movable.h
@@ -0,0 +1,31 @@
+//
+// Created by roman on 27.08.2020.
+//
+
+#ifndef FLASH_MOVABLE_H
+#define FLASH_MOVABLE_H
+
+namespace ObjectCapabilities {
+
+    /**
+     * @brief The base class of the Movable class hierarchy.
+     * @namespace ObjectCapabilities
+     *
+     * This class defines base Movable interface.
+    */
+    class Movable {
+    public:
+        Movable() = default;
+
+        /**
+         * @brief Simple interface method to move object to current coordinates.
+         * @param x coordinate.
+         * @param y coordinate.
+         */
+        virtual void move(int x, int y) noexcept = 0;
+
+        virtual ~Movable() = default;
+    };
+}
+
+#endif //FLASH_MOVABLE_H
