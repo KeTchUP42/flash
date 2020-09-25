813bee938d293804c23711fa1f721f2959b1203f
Roman Bondarenko <rom_bon@mail.ru>
2020-09-25 22:35:54 +0300

    Area Trigger interface added.

---
+++ engine/main/objects/static/triggers/area/AreaTrigger.h
@@ -0,0 +1,28 @@
+//
+// Created by roman on 25.09.2020.
+//
+
+#ifndef FLASH_AREATRIGGER_H
+#define FLASH_AREATRIGGER_H
+
+#include "../Trigger.h"
+#include "../../../auxiliary/components/elementary/Area.h"
+
+namespace Triggers {
+
+    /**
+     * @brief The base class of the AreaTrigger class hierarchy. Base interface class for area triggers.
+     * @namespace Triggers
+     *
+     * This class defines base AreaTrigger interface.
+    */
+    class AreaTrigger : public Trigger {
+    public:
+        AreaTrigger(const Components::Area &area) : _area(area) {}
+
+    protected:
+        Components::Area _area;
+    };
+}
+
+#endif //FLASH_AREATRIGGER_H
